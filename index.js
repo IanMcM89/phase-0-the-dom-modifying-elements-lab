2d874b56dad717ef3ac62d94802aeaaf2c9518d8
Ian <ianmcm.89@gmail.com>
2021-09-02 01:24:32 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Write your code here!
+//document.querySelector('main#main').remove();
+
+//document.body.removeChild(main);
+
+main.remove()
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'Ian is the champion';
+
+document.body.append(newHeader);
